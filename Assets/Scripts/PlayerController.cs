3ed3f7112645bf9bb9a73ad0e98303d1c3e05e73
Boris Farber <boris.farber@gmail.com>
2019-07-12 09:56:49 +0300

    Init drop

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Boris.Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 10.0f;
+        
+        void Update()
+        {
+            MovePlayerKeys();
+            MovePlayerTouch();
+        }
+        
+        private void MovePlayerKeys()
+        {
+            transform.position += 
+                Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed +
+                Vector3.up * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log(other.gameObject.tag);
+        }
+
+        private void MovePlayerTouch()
+        {
+            var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector3.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+    }
+}
+
+
+
+
+
+
+
