d3d1e9594c0c1700e7b34d622729ff93a9378fa8
shaharbar2 <shaharbar2@gmail.com>
2019-08-26 10:19:33 +0300

    ads initial commit

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Boris.Game
+{
+    public class AdsManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ShowVideoAd();
+        }
+
+        public void ShowVideoAd()
+        {
+            Advertisement.Show();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Boris.Game
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        public PoolManager pool;
+        public AdsManager Ads;
+        
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(this);
+            }
+
+            instance = this;
+            //DontDestroyOnLoad(this);
+        }
+    }
+}
